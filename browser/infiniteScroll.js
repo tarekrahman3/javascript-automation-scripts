16aecd2bc40342934522bc83c91a0130d3efcd92
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2023-10-16 20:31:00 +0600

    Create infiniteScroll.js

---
+++ browser/infiniteScroll.js
@@ -0,0 +1,15 @@
+// window.xpath = $x;
+let DATA_XPATH = "XPATH";
+let loadedData;
+while (true) {
+        window.scrollTo({
+                top: Number(document.body.scrollHeight),
+                left: 0,
+                behavior: 'smooth'
+        });
+        loadedData=window.xpath(DATA_XPATH).length;
+        console.log(loadedPosts.length);
+        document.querySelector('.load-more-wrapper > button').scrollIntoView()
+        document.querySelector('.load-more-wrapper > button').click()
+        await new Promise(r => setTimeout(r, 2000));
+};
